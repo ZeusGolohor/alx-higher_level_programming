6c087de853fa3a611a69f397600bbdcc7b9c2f37
ZeusGolohor <golohorzeusufuoma@gmail.com>
2022-12-04 07:49:34 -0800

    Write a program that prints all possible different combinations of two digits.
    Numbers must be separated by ,, followed by a space
    The two digits must be different
    01 and 10 are considered the same combination of the two digits 0 and 1
    Print only the smallest combination of two digits
    Numbers should be printed in ascending order, with two digits
    The last number should be followed by a new line
    You can only use no more than 3 print functions with string format
    You can only use no more than 2 loops in your code
    You are not allowed to store numbers or strings in a variable
    You are not allowed to import any module

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for f in range(0, 9):
+    for s in range(1, 10):
+        if (f != s and f < s):
+            # print('{}{}'.format(f, s), end=', ')
+            if ('{}{}'.format(f, s) != '89'):
+                print('{}{}'.format(f, s), end=', ')
+            elif ('{}{}'.format(f, s) == '89'):
+                print('{}{}'.format(f, s))
