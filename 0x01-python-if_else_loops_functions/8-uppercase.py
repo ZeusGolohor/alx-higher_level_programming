3ac10debdebf55b1a25353f82505fd5a555b59ed
ZeusGolohor <golohorzeusufuoma@gmail.com>
2022-12-04 15:20:37 -0800

    Write a function that prints a string in uppercase followed by a new line.
    Prototype: def uppercase(str):
    You can only use no more than 2 print functions with string format
    You can only use one loop in your code
    You are not allowed to import any module
    You are not allowed to use str.upper() and str.isupper()
    Tips: ord()
    You don’t need to understand __import__

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def uppercase(str):
-    print('{}'.format(str.upper()))
+    for i in str:
+        if (ord(i) >= 65 and ord(i) <= 90):
+            print('{}'.format(i), end='')
+        elif (ord(i) >= 97 and ord(i) <= 122):
+            print('{}'.format(chr(ord(i) - 32)), end='')
+        elif (ord(i) == 32):
+            print(' ', end='')
+        else:
+            print('{}'.format(i), end='')
+    print()
