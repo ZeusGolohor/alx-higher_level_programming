e6c462cefd6804ca76c02879db90ff2ffd3b50c7
ZeusGolohor <golohorzeusufuoma@gmail.com>
2022-12-09 19:13:25 -0800

    Write a function that removes all characters c and C from a string.
    Prototype: def no_c(my_string):
    The function should return the new string
    You are not allowed to import any module
    You are not allowed to use str.replace()

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,8 +6,10 @@ def print_matrix_integer(matrix=[[]]):
                 print('{:d}'.format(i[x]), end=' ')
             elif (x == (len(i) - 1)):
                 print('{:d}'.format(i[x]))
-    if (len(matrix) == 1):
-        print()
+    if (len(matrix) > 0):
+        if (len(matrix[0]) != 0):
+            print()
+        #print(len(matrix[0]))
 
 
 if __name__ == '__main__':
