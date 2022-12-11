43f26d6b4d2b767b03781bf6ad36e6e909fa4303
ZeusGolohor <golohorzeusufuoma@gmail.com>
2022-12-10 20:37:15 -0800

    Technical interview preparation:
    You are not allowed to google anything
    Whiteboard first
    Write a function in C that checks if a singly linked list is a palindrome.
    
    Prototype: int is_palindrome(listint_t **head);
    Return: 0 if it is not a palindrome, 1 if it is a palindrome
    An empty list is considered a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -15,3 +15,55 @@ int is_palindrome(listint_t **head)
     check = ple_check(*head, len);
     return (check);
 }
+
+/**
+  * list_len - Gets the length of a linked int.
+  * @head: head to linked list.
+  * Return: length of linked list.
+  */
+
+int list_len(const listint_t *head)
+{
+    int i;
+
+    i = -1;
+    while (head != NULL)
+    {
+        i++;
+        head = head->next;
+    }
+    return (i);
+}
+
+/**
+  * ple_check - to check if a list is a palindrome.
+  * @head: head to the linked list.
+  * @list_len: length of the list.
+  * Return: Bool.
+  */
+
+int ple_check(listint_t *head, int list_len)
+{
+    int array_a[list_len];
+    int array_b[list_len];
+    int i;
+    int x;
+
+    i = 0;
+    while (head != NULL)
+    {
+        x = head->n;
+        array_a[i] = x;
+        array_b[(list_len - i)] = x;
+        i++;
+        head = head->next;
+    }
+    i = 0;
+    while (i <= list_len)
+    {
+        if (array_a[i] != array_b[i])
+            return (0);
+        i++;
+    }
+    return (1);
+}
