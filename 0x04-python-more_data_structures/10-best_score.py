84444dd6170881ead90e7877055c9f1a8d010f02
ZeusGolohor <golohorzeusufuoma@gmail.com>
2022-12-20 19:15:39 -0800

    Write a function that returns a key with the biggest integer value.
    Prototype: def best_score(a_dictionary):
    You can assume that all values are only integers
    If no score found, return None
    You can assume all students have a different score
    You are not allowed to import any module

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@
 def best_score(a_dictionary):
     best = 0
     name = ''
-    if a_dictionary is None or a_dictionary is {}:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return (None)
     for i in list(a_dictionary):
         if (a_dictionary[i] > best):
