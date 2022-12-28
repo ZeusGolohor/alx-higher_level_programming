10cac6ba2d5ff593a802e02605df7e3b46b11d7c
ZeusGolohor <golohorzeusufuoma@gmail.com>
2022-12-27 19:36:24 -0800

    Write a class Square that defines a square by: (based on 0-square.py)
    Private instance attribute: size
    Instantiation with size (no type/value verification)
    You are not allowed to import any module
    Why?
    
    Why size is private attribute?
    
    The size of a square is crucial for a square, many things depend of it (area computation, etc.), so you, as class builder, must control the type and value of this attribute. One way to have the control is to keep it privately. You will see in next tasks how to get, update and validate the size value.

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+The Square class is an empty class that is used
+to demonstrate how classes works
+"""
+
+
+class Square:
+    """
+    The Square class is an empty class that is used
+    to demonstrate how classes works
+    """
+    def __init__(self, size):
+        self.__size = size
