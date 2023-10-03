eb0bc29803e8f5842ff6a7b7e2694e92c520b0c2
golohorzeus <golohorzeusufuoma@gmail.com>
2023-10-02 19:53:41 -0700

    Technical interview preparation:
    You are not allowed to google anything
    Whiteboard first
    This task and all future technical interview prep tasks will include checks for the efficiency of your solution, i.e. is your solution’s runtime fast enough, does your solution require extra memory usage / mallocs, etc.
    Write a function in C that checks if a singly linked list has a cycle in it.
    
    Prototype: int check_cycle(listint_t *list);
    Return: 0 if there is no cycle, 1 if there is a cycle
    Requirements:
    
    Only these functions are allowed: write, printf, putchar, puts, malloc, free

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+	listint_t *head = list;
+	while (list->next != NULL)
+	{
+		if (list->next == head)
+			return (1);
+		list = list->next;
+	}
+	return (0);
+}
