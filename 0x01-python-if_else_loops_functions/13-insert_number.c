adc8bb4e8a5821d1f0dd94507f98dc44fbaed424
ZeusGolohor <golohorzeusufuoma@gmail.com>
2023-10-21 09:33:32 -0700

    Technical interview preparation:
    You are not allowed to google anything
    Whiteboard first
    Write a function in C that inserts a number into a sorted singly linked list.
    
    Prototype: listint_t *insert_node(listint_t **head, int number);
    Return: the address of the new node, or NULL if it failed

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,12 +1,65 @@
-#include <lists.h>
+#include "lists.h"
 
+/**
+ * insert_node - A function used to insert a newnode into a list.
+ * @head: a pointer to a pointer to the head node.
+ * @number: the data to be inserted into the newnode.
+ * Return: listint_t *.
+ */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *temp;
+	listint_t *temp, *newnode;
 
 	if (head == NULL)
-		return (NULL)
+		return (NULL);
 	temp = *head;
-	while (temp != NULL)
-	
+	if (number <= temp->n)
+	{
+		newnode = create_newnode(number, temp);
+		if (newnode == NULL)
+			return (NULL);
+		*head = newnode;
+	}
+	else
+	{
+		while (temp->next != NULL)
+		{
+			if (number <= temp->next->n)
+			{
+				newnode = create_newnode(number, temp->next);
+				if (newnode == NULL)
+					return (NULL);
+				temp->next = newnode;
+				break;
+			}
+			temp = temp->next;
+		}
+		if (temp->next == NULL)
+		{
+			newnode = create_newnode(number, NULL);
+			if (newnode == NULL)
+				return (NULL);
+			temp->next = newnode;
+		}
+	}
+	return (*head);
 }
+
+/**
+ * create_newnode - A function used to create newnodes.
+ * @n: data to be inserted into the newnode.
+ * @next: next node to be inserted into the list.
+ * Return: listint_t *.
+ */
+listint_t *create_newnode(int n, listint_t *next)
+{
+	listint_t *newnode;
+
+	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = n;
+	newnode->next = next;
+	return (newnode);
+}
+
