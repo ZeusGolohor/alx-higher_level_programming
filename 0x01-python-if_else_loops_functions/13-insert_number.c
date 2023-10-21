4d713ed839cc59d814a604729ad54696395ebe64
ZeusGolohor <golohorzeusufuoma@gmail.com>
2023-10-21 09:40:50 -0700

    Technical interview preparation:
    You are not allowed to google anything
    Whiteboard first
    Write a function in C that inserts a number into a sorted singly linked list.
    
    Prototype: listint_t *insert_node(listint_t **head, int number);
    Return: the address of the new node, or NULL if it failed

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -10,7 +10,7 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *temp, *newnode;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (NULL);
 	temp = *head;
 	if (number <= temp->n)
