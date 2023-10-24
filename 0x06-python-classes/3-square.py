41101c2bc48f23b4be3d313b52d655c639896c68
ZeusGolohor <golohorzeusufuoma@gmail.com>
2023-10-24 12:18:29 -0700

    Write a class Square that defines a square by: (based on 2-square.py)
    Private instance attribute: size
    Instantiation with optional size: def __init__(self, size=0):
    size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
    if size is less than 0, raise a ValueError exception with the message size must be >= 0
    Public instance method: def area(self): that returns the current square area
    You are not allowed to import any module

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+The Square class is an empty class that is used
+to demonstrate how classes works
+"""
+
+
+class Square:
+    """
+    The Square class is an empty class that is used
+    to demonstrate how classes works
+    """
+    def __init__(self, size=0):
+        self.__size_att = size
+
+    @property
+    def __size_att(self):
+        return (self.__size)
+
+    @__size_att.setter
+    def __size_att(self, x):
+        if isinstance(x, int) is False:
+            raise TypeError("size must be an integer")
+        else:
+            if x < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = x
+    def area(self):
+        return (self.__size * self.__size)
