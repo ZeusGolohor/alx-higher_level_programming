58275e2762b4a5c8aec9245ce4dc7c1965bfd6ce
ZeusGolohor <golohorzeusufuoma@gmail.com>
2023-10-31 14:17:11 -0700

    Refactor

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,9 +3,8 @@ class LockedClass:
     def __init__(self):
         print(dir(self))
         for word in dir(self):
-            if (word[0] != '_' and word [1] != '_'):
+            if (word[0] != '_' and word[1] != '_'):
                 print(word)
                 if (word != 'first_name'):
                     raise AttributeError()
                     break
-
