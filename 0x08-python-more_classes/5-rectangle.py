df6c1a3cfab3748538d41f05e4ac89e57db35bd4
ZeusGolohor <golohorzeusufuoma@gmail.com>
2023-11-01 19:08:50 -0700

    Write a class Rectangle that defines a rectangle by: (based on 4-rectangle.py)
    Private instance attribute: width:
    property def width(self): to retrieve it
    property setter def width(self, value): to set it:
    width must be an integer, otherwise raise a TypeError exception with the message width must be an integer
    if width is less than 0, raise a ValueError exception with the message width must be >= 0
    Private instance attribute: height:
    property def height(self): to retrieve it
    property setter def height(self, value): to set it:
    height must be an integer, otherwise raise a TypeError exception with the message height must be an integer
    if height is less than 0, raise a ValueError exception with the message height must be >= 0
    Instantiation with optional width and height: def __init__(self, width=0, height=0):
    Public instance method: def area(self): that returns the rectangle area
    Public instance method: def perimeter(self): that returns the rectangle perimeter:
    if width or height is equal to 0, perimeter has to be equal to 0
    print() and str() should print the rectangle with the character #:
    if width or height is equal to 0, return an empty string
    repr() should return a string representation of the rectangle to be able to recreate a new instance by using eval()
    Print the message Bye rectangle... (... being 3 dots not ellipsis) when an instance of Rectangle is deleted
    You are not allowed to import any module

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""
+This module is a simple Rectangle module.
+"""
+
+
+class Rectangle:
+    """
+    This is a simple rectangle class
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if (isinstance(value, int) is False):
+            raise TypeError("width must be an integer")
+        elif (value < 0):
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if (isinstance(value, int) is False):
+            raise TypeError("height must be an integer")
+        elif (value < 0):
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return (self.width * self.height)
+
+    def perimeter(self):
+        if ((self.width == 0) or (self.height == 0)):
+            return (0)
+        return (2 * (self.width + self.height))
+
+    def __str__(self):
+        """
+        Used to return a nicely printed user friendly
+        string.
+        """
+        string = ''
+        if (self.__height == 0):
+            return (string)
+        if (self.__width == 0):
+            return (string)
+
+        for i in range(self.__height):
+            for x in range(self.__width):
+                string = string + '#'
+            if ((i + 1) != self.__height):
+                string = string + '\n'
+        return (string)
+
+    def __repr__(self):
+        return ("Rectangle(" + str(self.width) + ", " + str(self.height) + ")")
+
+    def __del__(self):
+        print("Bye rectangle...")
