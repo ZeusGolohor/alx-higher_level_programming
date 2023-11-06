d9f2a15b6166d9e8266dc87a7c1f766e76d65cce
ZeusGolohor <golohorzeusufuoma@gmail.com>
2023-11-06 14:00:48 -0800

    Refactor

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
 This module is used to check if the object is an instance of
-a class that inherited (directly or indirectly) from the specified class.
+a class that inherited (directly or indirectly) 4rm the specified class.
 """
 
 
 def inherits_from(obj, a_class):
     """
     This module is used to check if the object is an instance of
-    a class that inherited (directly or indirectly) from the specified class.
+    a class that inherited (directly or indirectly) 4rm the specified class.
     """
     return (not(type(obj) is a_class) and isinstance(obj, a_class))
