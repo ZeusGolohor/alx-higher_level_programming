70c4a98bdcd8a173bae0cf9d54d691f3eccd9764
root <zeusgolohor@gmail.com>
2023-12-12 19:42:00 -0800

    Write a script that deletes the database hbtn_0c_0 in your MySQL server.
    If the database hbtn_0c_0 doesn’t exist, your script should not fail
    You are not allowed to use the SELECT or SHOW statements

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- A script to create a database if it dies not exit
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- A script to create a database if it dies not exit.
+DROP DATABASE IF EXISTS hbtn_0c_0;
