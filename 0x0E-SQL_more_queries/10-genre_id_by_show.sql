d22918ee3b515760156a8f759baebfce7aa8cfe8
root <zeusgolohor@gmail.com>
2023-12-26 08:59:32 -0800

    Import the database dump of hbtn_0d_tvshows to your MySQL server: download (same as 10-genre_id_by_show.sql)
    Write a script that lists all shows contained in the database hbtn_0d_tvshows.
    
    Each record should display: tv_shows.title - tv_show_genres.genre_id
    Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
    If a show doesn’t have a genre, display NULL
    You can use only one SELECT statement
    The database name will be passed as an argument of the mysql command

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,1 +1,2 @@
+-- Write a script that lists all shows contained in hbtn_0d_tvshows
 SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id FROM tv_show_genres LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
