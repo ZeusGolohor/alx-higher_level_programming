df26b39919798f566dbb9377ff0e5d5e4dee599b
root <zeusgolohor@gmail.com>
2024-01-13 18:06:08 -0800

    Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This script is used to lists all states from the database hbtn_0e_0_usa:
+"""
+import sys
+import MySQLdb
+
+
+def run():
+    usr = sys.argv[1]
+    pwd = sys.argv[2]
+    dbe = sys.argv[3]
+    name = sys.argv[4]
+    conn = MySQLdb.connect(
+                            host="localhost",
+                            port=3306,
+                            user=usr,
+                            passwd=pwd,
+                            db=dbe,
+                            charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name\
+        LIKE '{}' ORDER BY states.id ASC".format(name))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    run()
