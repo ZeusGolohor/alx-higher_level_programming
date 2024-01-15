07c21af30929f7e9e86a4aa0245405bb22da0e63
golohorzeus <golohorzeusufuoma@gmail.com>
2024-01-14 19:12:01 -0800

    Write a script that prints the first State object from the database hbtn_0e_6_usa
    Your script should take 3 arguments: mysql username, mysql password and database name
    You must use the module SQLAlchemy
    You must import State and Base from model_state - from model_state import Base, State
    Your script should connect to a MySQL server running on localhost at port 3306
    The state you display must be the first in states.id
    You are not allowed to fetch all states from the database before displaying the result
    The results must be displayed as they are in the example below
    If the table states is empty, print Nothing followed by a new line
    Your code should not be executed when imported

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -16,11 +16,9 @@ if __name__ == '__main__':
         sys.argv[2],
         sys.argv[3]),
         pool_pre_ping=True)
-
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).order_by(State.id).first()
+    state = session.query(State).first()
     if state:
         print("{}: {}".format(state.id, state.name))
     else:
