12b4a298edab1a9e52b9f6fce702ef9795127d45
golohorzeus <golohorzeusufuoma@gmail.com>
2024-01-14 16:41:38 -0800

    Write a python file that contains the class definition of a State and an instance Base = declarative_base():
    State class:
    inherits from Base Tips
    links to the MySQL table states
    class attribute id that represents a column of an auto-generated, unique integer, can’t be null and is a primary key
    class attribute name that represents a column of a string with maximum 128 characters and can’t be null
    You must use the module SQLAlchemy
    Your script should connect to a MySQL server running on localhost at port 3306
    WARNING: all classes who inherit from Base must be imported before calling Base.metadata.create_all(engine)

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,14 +3,8 @@
 This script that contains the class definition
 of a State and an instance Base = declarative_base():
 """
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import (
-                        Column,
-                        Integer,
-                        String,
-                        create_engine,
-                        MetaDate
-                        )
 
 mtd = MataData()
 Base = declarative_base(metadata=mtd)
