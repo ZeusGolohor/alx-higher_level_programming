69c1c97eb86b4f3abb6082a2827383bac4899365
golohorzeus <golohorzeusufuoma@gmail.com>
2024-01-14 18:48:13 -0800

    Write a script that lists all State objects from the database hbtn_0e_6_usa
    Your script should take 3 arguments: mysql username, mysql password and database name
    You must use the module SQLAlchemy
    You must import State and Base from model_state - from model_state import Base, State
    Your script should connect to a MySQL server running on localhost at port 3306
    Results must be sorted in ascending order by states.id
    The results must be displayed as they are in the example below
    Your code should not be executed when imported

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,10 +5,12 @@ This script is used to lists all State objects from the database hbtn_0e_6_usa
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
 
 
 if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        sys.exit(1)
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1],
         sys.argv[2],
@@ -16,7 +18,8 @@ if __name__ == '__main__':
         pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
-    sss = sessionmaker(bind=engine)
-    session = sss()
-    for state in session.query(State).order_by(asc(State.id)):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(asc(State.id):
         print("{}: {}".format(state.id, state.name))
+    session.close()
