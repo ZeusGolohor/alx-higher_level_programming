4d8d8f7455e824ca404af31c2b8e0aa7a2d0dfd6
golohorzeus <golohorzeusufuoma@gmail.com>
2024-01-14 18:06:59 -0800

    Write a script that lists all State objects from the database hbtn_0e_6_usa
    Your script should take 3 arguments: mysql username, mysql password and database name
    You must use the module SQLAlchemy
    You must import State and Base from model_state - from model_state import Base, State
    Your script should connect to a MySQL server running on localhost at port 3306
    Results must be sorted in ascending order by states.id
    The results must be displayed as they are in the example below
    Your code should not be executed when imported

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,27 +2,19 @@
 """
 This script is used to lists all State objects from the database hbtn_0e_6_usa
 """
-from model_state import Base, State
+import sys
+from model_state import State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import sys
 
 
-def run():
-    """
-    This is a method to make sure code dont run unless its needed
-    """
+if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1],
         sys.argv[2],
         sys.argv[3]),
         pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    sss = sessionmaker(bind=engine)
+    session = sss()
     for state in session.query(State).order_by(asc(State.id)):
         print('{}: {}'.format(state.id, state.name))
-
-
-if __name__ == '__main__':
-    run()
-    session.close()
