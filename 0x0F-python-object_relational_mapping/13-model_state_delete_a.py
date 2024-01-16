82ba3db54f1caae2fe2ce43f732a22efddfc6ed2
root <zeusgolohor@gmail.com>
2024-01-15 18:02:29 -0800

    Write a script that deletes all State objects with a name containing the letter a from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
     states = session.query(State).filter(
-        State.name.like('%a%')
+        State.name.like('%a')
     )
     for state in states:
         session.delete(state)
