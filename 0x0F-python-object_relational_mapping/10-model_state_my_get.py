28336f7d293bba24d1b9e60d92db42fc9acd5c2b
root <zeusgolohor@gmail.com>
2024-01-15 17:43:32 -0800

    Write a script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     state = session.query(State).filter(
         State.name == name
     ).order_by(State.id).first()
-    if state is None:
+    if state is not None:
         print("{}".format(state.id))
     else:
         print("Not found")
