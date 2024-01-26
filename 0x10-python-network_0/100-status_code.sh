a047fe356fb855064cc9fc868a54531a6983a926
Zeus Golohor <zeusgolohor@gmail.com>
2024-01-25 16:23:16 -0800

    Write a Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
    You are not allowed to use any pipe, redirection, etc.
    You are not allowed to use ; and &&

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s --head "$1" | grep -i "HTTP/1.1" | cut -d ' ' -f 2
+curl -s --head -w "%{http_code}" -o /dev/null "$1"
