8ed823be0932f17b62d92cea9408bebe90431bed
root <zeusgolohor@gmail.com>
2024-01-26 15:40:33 -0800

    Write a Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response.
    You must use the packages urllib and sys
    You are not allow to import packages other than urllib and sys
    The value of this variable is different for each request
    You don’t need to check arguments passed to the script (number or type)
    You must use a with statement

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,7 +5,13 @@ Python script that fetches https://alx-intranet.hbtn.io/status.
 import urllib.request
 import sys
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    data = response.info()
-    print(data["X-Request-Id"])
+
+def run():
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        data = response.info()
+        print(data.get("X-Request-Id"))
+
+
+if __name__ == "__main__":
+    run()
